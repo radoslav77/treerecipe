a016cc081d008b13060f39770c9fbc4d8699c047
radoslav77 <rado7711@yahoo.co.uk>
2021-10-30 13:44:30 +0100

    #0079

--- recipe/admin.py
+++ recipe/admin.py
@@ -4,18 +4,29 @@ from django.contrib.auth.models import User
 from .models import *
 # Register your models here.
 
+
 class UserAdmin(admin.ModelAdmin):
-    list_display=("id","username","email","password")
+    list_display = ("id", "username", "email", "password")
+
 
 class RecipeAdmin(admin.ModelAdmin):
-    list_display=('id','title','recipe','method','outlet', 'image')
-admin.site.register(Recipe,RecipeAdmin)
+    list_display = ('id', 'title', 'recipe', 'method',
+                    'outlet', 'image', 'archived')
+
+
+admin.site.register(Recipe, RecipeAdmin)
+
 
 class HandoverAdmin(admin.ModelAdmin):
-    list_display=('id','user','msg', 'date')
-admin.site.register(Handover,HandoverAdmin)
+    list_display = ('id', 'user', 'msg', 'date')
+
+
+admin.site.register(Handover, HandoverAdmin)
+
 
 class Sub_recipeAdmin(admin.ModelAdmin):
-    list_display = ('id','title','sub_title','sub_recipe', 'method')
+    list_display = ('id', 'title', 'sub_title',
+                    'sub_recipe', 'method', 'archived')
 
-admin.site.register(Sub_recipe,Sub_recipeAdmin)
+
+admin.site.register(Sub_recipe, Sub_recipeAdmin)
